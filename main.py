2a7324470b6e1a7e8e557c2bf7b70ba8b415a2d0
Vinicius <Viniciushmenezes@hotmail.com>
2025-10-29 18:22:21 -0300

    Produto para Veiculo em database
    Alteração de chamada

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from database import Base, engine
-from produtos import Produto
+from produtos import Veiculo
 
 print("Criando banco de dados..")
 Base.metadata.create_all(bind=engine)
 print("Banco de dados criado com sucesso!")
+
